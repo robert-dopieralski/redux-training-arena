20c24c0a8eea238be48535c9fbd7feb5469384ee
Robert Dopieralski <abc507446692@gmail.com>
2019-03-04 23:17:20 +0100

    some fancy destructurisation made here

--- src/duck/counter.js
+++ src/duck/counter.js
@@ -20,20 +20,22 @@ const resetAction = {
   type: RESET
 };
 
+//initial state value for reset function
 const initialValue = 0;
+
+//predicates
+const predicates = {
+  [INCREMENT]: state => state + 1,
+  [DECREMENT]: state => state - 1,
+  [RESET]: () => initialValue
+};
+
+//reducer warmup function ;-)
+const identity = x => x;
 
 //REDUCER BE LIKE : (oldState, action) => newState
 function counterReducer(state = initialValue, action) {
-  if (action.type === INCREMENT) {
-    return state + 1;
-  }
-  if (action.type === DECREMENT) {
-    return state - 1;
-  }
-  if (action.type === RESET) {
-    return initialValue;
-  }
-  return state;
+  return (predicates[action.type] || identity)(state, action);
 }
 
 export const store = createStore(counterReducer);
